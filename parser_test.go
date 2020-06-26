d2c2b0962eeb0831a90cd7a36157c4da649f09fd
Gifary Dhimas <gifarydhimas@gmail.com>
2020-06-26 08:52:05 +0700

    Add more test for checkRequiredArgs

--- parser_test.go
+++ parser_test.go
@@ -16,29 +16,39 @@ func TestCheckRequiredArgs(t *testing.T) {
 
 	testCases := []struct {
 		name     string
+		cmd      string
 		args     []string
 		expected bool
 	}{
 		{
 			name:     "args is less than required",
+			cmd:      "test",
 			args:     []string{"1", "3", "5"},
 			expected: false,
 		},
 		{
 			name:     "args is more than required",
+			cmd:      "test",
 			args:     []string{"1", "3", "5", "1", "3", "5", "5"},
 			expected: true,
 		},
 		{
 			name:     "args is less than required",
+			cmd:      "test",
 			args:     []string{"1", "3", "5", "5", "5"},
 			expected: true,
+		},
+		{
+			name:     "cmd is not registered",
+			cmd:      "invalid",
+			args:     []string{"1", "3", "5"},
+			expected: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := checkRequiredArgs("test", tc.args)
+			got := checkRequiredArgs(tc.cmd, tc.args)
 
 			assert.Equal(t, tc.expected, got)
 		})
